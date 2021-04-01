6fb83dce94e47d75085a755bbf992f161657d1a7
Innuendo416 <collinstone2291@icloud.com>
2021-04-01 15:49:52 -0400

    Pages laid out

---
+++ script.js
@@ -0,0 +1,8 @@
+/*const slides = Array.from(document.querySelector('.slideShow').children)
+const startSlideShow = (arr, n) => {
+
+let slideIndex = 0
+
+window.addEventListener('DOMContentLoaded', () => {
+	startSlideShow(slides, slideIndex)
+})*/
